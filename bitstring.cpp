32e7f877a2c00fd2d76bd06b31bcc5c032061103
Alexey Snytnikov <snytav@gmail.com>
2024-06-04 00:37:55 +0200

    uns long long OK

--- bitstring.cpp
+++ bitstring.cpp
@@ -14,8 +14,8 @@ void printStringBySymbol(char* s)
 }
 
 char* toBinaryString(unsigned long long n) {
-    char string[33];// = (char*)malloc(sizeof(int) * 8 + 1);
-    strcpy_s(string, sizeof(int) * 8 + 1, "");
+    char string[65];// = (char*)malloc(sizeof(int) * 8 + 1);
+    strcpy_s(string, 65, "");
     printf("\n");
 
     if (!string) {
@@ -23,12 +23,12 @@ char* toBinaryString(unsigned long long n) {
     }
 
     // 1234567812345678
-    strcpy_s(string, sizeof(int) * 8 + 1, "                                ");
-    string[32] = 0;
-    for (int i = 31; i >= 0; i--) {
+    strcpy_s(string, 64, "                                ");
+    string[64] = 0;
+    for (int i = 63; i >= 0; i--) {
         string[i] = (n & 1) + '0';
         //n >> 1;
-        printf("2str n %5d i %2d n&1 %d string[i] %c s %32s\n", n, i, n & 1, string[i], string);
+        printf("2str n %10ull i %2d n&1 %d string[i] %c s %32s\n", n, i, n & 1, string[i], string);
         //printStringBySymbol(string);
        
         n = n >> 1;
@@ -36,14 +36,24 @@ char* toBinaryString(unsigned long long n) {
     return string;
 }
 
+void LongToBinaryString(unsigned long long n,char *s)
+{
+    char *hi,*lo;
+    lo = toBinaryString(n & 0xffffffff);
+    hi = toBinaryString(n & 0xffffffff00000000ull);
+    strcpy_s(s,128, hi);
+    strcat_s(s,128, lo);
+}
 
 
 int main()
 {
-    char s[100];
-    strcpy_s(s, "12345678abcdefgh");
-    printStringBySymbol(s);
-    char* conv_s = toBinaryString(0xabffafaf);
+    char s[128];
+    //strcpy_s(s, "12345678abcdefgh");
+    //printStringBySymbol(s);
+    char* conv_s = toBinaryString(0xabffafafab1fafaf);
+    //LongToBinaryString(0xabffafafab1fafaf,s);
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
